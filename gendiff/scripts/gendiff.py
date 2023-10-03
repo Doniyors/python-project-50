dcd4ce52f63f060267399379be3d685e48e2adcf
Doniyor <doniyorshavkati@gmail.com>
2023-10-03 10:19:19 +0500

    almost last push

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,37 +1,14 @@
 #!/usr/bin/env python3
 
-import argparse
-
-from gendiff.json_diff import generate_diff
+from gendiff.generate_diff import generate_diff
+from gendiff.tools.argparse import parser_arg
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Compares two configuration files and shows a difference."
-    )
-    parser.add_argument("first_file", help="path to the first file")
-    parser.add_argument("second_file", help="path to the second file")
-    parser.add_argument("-f", "--format", help="set format of output")
-    args = parser.parse_args()
-
-    print("Comparing files:", args.first_file, args.second_file)
-
-    file1_path = "file1.json"
-    file2_path = "file2.json"
-
-    diff = generate_diff(file1_path, file2_path)
-    print(diff)
+    path_file1, path_file2, format_name = parser_arg()
+    result = generate_diff(path_file1, path_file2, format_name)
+    print(result)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
-
-def some_foo(x):
-    for i in x:
-        if i == 0:
-            x[-1] = i
-
-
-e = [0, 1, 0, 2, 0, 4]
-print(some_foo(e))
